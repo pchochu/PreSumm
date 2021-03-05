8d7491e3e83ff5d113409c440c2fbf020075e294
Peter <pchochula@gmail.com>
2021-03-05 09:41:04 +0100

    a

--- src/train.py
+++ src/train.py
@@ -21,6 +21,7 @@ if __name__ == '__main__':
     parser.add_argument("-model_path", default='../models/')
     parser.add_argument("-result_path", default='../results/cnndm')
     parser.add_argument("-temp_dir", default='../temp')
+    parser.add_argument("-text", default='')
     parser.add_argument("-text_src", default='')
     parser.add_argument("-text_tgt", default='')
 
